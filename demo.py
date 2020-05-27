39dbdd561d4380cfc49df05c548d6fc284bf293f
Neamat <nanaghareeb6@gmail.com>
2020-05-27 23:07:41 +0200

    initial commit

---
+++ demo.py
@@ -0,0 +1,15 @@
+import math
+import os
+import sys
+
+
+print(sys.version)
+# name = input("your name? ")
+# print("hello " + name)
+print("this u ok ")
+print("hi")
+print("hello")
+name = 15
+print(name)
+print("nana")
+
